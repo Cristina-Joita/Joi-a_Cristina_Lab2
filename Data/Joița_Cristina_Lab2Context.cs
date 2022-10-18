39f89a68f9b2fa3f829e73fadf5cbbeec0b8638a
CRISTINA <CRISTINA@DESKTOP-VKD0A3J>
2022-10-18 22:42:25 +0300

    Add project files.

---
+++ Data/Joița_Cristina_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Joița_Cristina_Lab2.Models;
+
+namespace Joița_Cristina_Lab2.Data
+{
+    public class Joița_Cristina_Lab2Context : DbContext
+    {
+        public Joița_Cristina_Lab2Context (DbContextOptions<Joița_Cristina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Joița_Cristina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Joița_Cristina_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Joița_Cristina_Lab2.Data;
+using Joița_Cristina_Lab2.Models;
+
+namespace Joița_Cristina_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Joița_Cristina_Lab2.Data.Joița_Cristina_Lab2Context _context;
+
+        public IndexModel(Joița_Cristina_Lab2.Data.Joița_Cristina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                    .Include(b => b.Publisher)
+                    .ToListAsync();
+
+            }
+        }
+    }
+}
