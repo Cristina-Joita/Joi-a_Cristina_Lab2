4e90c2b2dab4d6d503987e5282ad6fd1494b45b0
CRISTINA <cristina.joita@stud.ubbcluj.ro>
2022-11-09 15:55:03 +0200

    Lab05- fara 19

---
+++ Pages/Members/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Joița_Cristina_Lab2.Data;
+using Joița_Cristina_Lab2.Models;
+
+namespace Joița_Cristina_Lab2.Pages.Members
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Joița_Cristina_Lab2.Data.Joița_Cristina_Lab2Context _context;
+
+        public CreateModel(Joița_Cristina_Lab2.Data.Joița_Cristina_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Member Member { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Member.Add(Member);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
