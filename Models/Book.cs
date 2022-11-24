261325de0138198cdc5b87966ae2751777168967
CRISTINA <cristina.joita@stud.ubbcluj.ro>
2022-11-24 16:42:59 +0200

    Laborator 6 fara erori

--- Models/Book.cs
+++ Models/Book.cs
@@ -12,11 +12,11 @@ namespace Joița_Cristina_Lab2.Models
         [Display(Name = "Book Title")]
         public string Title { get; set; }
 
-        [Column(TypeName = "decimal(6, 2)")]
-        [Range(0.01, 500)]
+       
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
         [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
